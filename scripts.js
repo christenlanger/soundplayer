cebf8a0405fc7cf8975b97ced369564c5cf7f5b6
Eddryan Aranzanso <edd.aranzanso@gmail.com>
2025-06-10 10:20:02 +0800

    First commit

---
+++ scripts.js
@@ -0,0 +1,144 @@
+const { createApp, ref } = Vue;
+var ctx;
+var gainNode;
+var playSound;
+
+const songfile = "./songlist.json";
+const noSongText = "Hang tight!";
+
+createApp({
+    data() {
+        return {
+            songData: {},
+            current: {},
+            finished: [],
+            isAsking: false,
+            isLoading: false,
+            isPlaying: false,
+            curVolume: 50,
+            debugEnabled: false,
+            delayResult: 0
+        };
+    },
+    created() {
+        // Get config
+        fetch(songfile)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error: ${res.status}`)
+                }
+                return res.json();
+            })
+            .then((data) => {
+                this.songData = data;
+                this.debugEnabled = typeof data.debugMode !== 'undefined' ? data.debugMode : false;
+                this.delayResult = typeof data.delayResult !== 'undefined' ? data.delayResult : 0;
+            })
+            .catch((error) =>
+                console.error("Unable to fetch data:", error));
+    },
+    watch: {
+        curVolume(val) {
+            if (typeof gainNode !== 'undefined') {
+                gainNode.gain.value = val / 100;
+            }
+        }
+    },
+    computed: {
+        catList() {
+            return (typeof this.songData.cats !== 'undefined') ? this.songData.cats.map((obj) => {
+                return {
+                    name: obj.name,
+                    count: obj.songs.length
+                }
+            }) : [];
+        },
+        curSongName() {
+            if (typeof this.current.name === 'undefined') {
+                return noSongText;
+            }
+
+            return (this.isAsking) ? '[' + this.current.catname + '] ???' : this.current.name;
+        },
+        noSongLoaded() {
+            return this.isLoading || typeof this.current.catname === 'undefined';
+        }
+    },
+    methods: {
+        getSong(i, j) {
+            if (!this.isAsking) {
+                if (this.isPlaying) {
+                    ctx.close().then(() => this.loadSong(i, j));
+                    this.isPlaying = false;
+                }
+                else {
+                    this.loadSong(i, j);
+                }
+            }
+        },
+        revealSong() {
+            if (this.isAsking && !this.isPlaying) {
+                this.playSong();
+                setTimeout(() => {
+                    this.isAsking = false;
+
+                    this.finished.push(this.current);
+                }, this.delayResult);
+            }
+        },
+        playSong(time, beginning) {
+            if (!this.isPlaying) {
+                beginning = typeof beginning === 'undefined' ? false : beginning;
+                gainNode = ctx.createGain();
+                gainNode.connect(ctx.destination);
+                gainNode.gain.value = this.curVolume / 100;
+
+                playSound = ctx.createBufferSource();
+                playSound.buffer = this.current.audio;
+                playSound.connect(gainNode);
+                playSound.onended = () => {
+                    this.isPlaying = false;
+                };
+                playSound.start(ctx.currentTime, (beginning) ? 0 : this.current.timestamp, time);
+                this.isPlaying = true;
+            }
+        },
+        loadSong(i, j) {
+            let songs = this.songData.cats[i].songs;
+
+            if (songs.length > 0 && !this.isAsking) {
+                this.isAsking = true;
+
+                if (typeof j === 'undefined') {
+                    this.current = songs.splice(Math.floor(Math.random() * songs.length), 1)[0];
+                }
+                else {
+                    this.current = songs[j];
+                }
+                this.current.catname = this.songData.cats[i].name;
+
+                ctx = new AudioContext();
+                this.isLoading = true;
+                fetch(this.songData.path + "/" + this.current.src)
+                    .then(data => data.arrayBuffer())
+                    .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+                    .then(decodedAudio => {
+                        this.current.audio = decodedAudio;
+                        this.isLoading = false;
+                    })
+                    .catch((error) =>
+                        console.error("Unable to fetch data:", error));
+            }
+        },
+        toggleSong() {
+            if (typeof playSound !== 'undefined') {
+                if (this.isPlaying) {
+                    playSound.stop();
+                }
+                else {
+                    this.playSong(undefined, true);
+                }
+            }
+        }
+    }
+}).mount('#app');
